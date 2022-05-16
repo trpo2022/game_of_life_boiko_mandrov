bc2fa073c4a1b841b275e9b7a006e5916b1870c9
Faylaks <boyko.oleg03@mail.ru>
2022-05-16 21:53:53 +0700

    final_8_issue

---
+++ src/test/UITest.java
@@ -0,0 +1,35 @@
+package test;
+
+import game.Board;
+import game.UI;
+import org.junit.jupiter.api.Test;
+
+
+import java.awt.*;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class UITest {
+
+    @Test
+    void getBoard() {
+        int[][] f = new int[3][3];
+        UI a = new UI(f, 3, 3);
+        Board now = a.getBoard();
+        assertEquals(now.getHeight(), 3);
+    }
+
+    @Test
+    void start() {
+        int[][] f = new int[3][3];
+        UI a = new UI(f, 3, 3);
+        Component[] components = a.getComponents();
+        int count = 0;
+        for (Component ignored : components) {
+            count++;
+        }
+        assertEquals(count, 4);
+    }
+}
